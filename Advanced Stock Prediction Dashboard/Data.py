8b73621d726ae0308c5888112c4e2d8a5f584a54
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-11-07 23:28:05 +0530

    Update Data.py

--- Advanced Stock Prediction Dashboard/Data.py
+++ Advanced Stock Prediction Dashboard/Data.py
@@ -5,3 +5,7 @@ def fetch_price_data(tickers, start, end):
   all_data = {}
     for ticker in tickers:
         try:
+            df = yf.download(ticker, start=start, end=end, progress=False, auto_adjust=False)
+            if df.empty: # type: ignore
+                print(f"No data for {ticker}, skipping...")
+                continue
