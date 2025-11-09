6678374912ee2026d7232adf22f41b4c514571b0
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-11-09 21:42:38 +0530

    Update features.py

--- Advanced Stock Prediction Dashboard/features.py
+++ Advanced Stock Prediction Dashboard/features.py
@@ -4,3 +4,8 @@ import numpy as np
 # --- Basic indicators ---
 def ema(series, span):
     return series.ewm(span=span, adjust=False).mean()
+
+def rsi(series, length=14):
+    delta = series.diff()
+    up = delta.clip(lower=0)
+    down = -1 * delta.clip(upper=0)
