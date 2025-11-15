a280a3ea3c86e949b92ea9bb09152422046969dc
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-11-16 00:11:49 +0530

    Update features.py

--- Advanced Stock Prediction Dashboard/features.py
+++ Advanced Stock Prediction Dashboard/features.py
@@ -36,3 +36,6 @@ def atr(df, window=14):
     high_low = df['High'] - df['Low']
     high_close = (df['High'] - df['Close'].shift()).abs()
     low_close = (df['Low'] - df['Close'].shift()).abs()
+    tr = pd.concat([high_low, high_close, low_close], axis=1).max(axis=1)
+    return tr.rolling(window).mean()
+
