e85e4b34079122678fe4816ac6445ee8d14f2807
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-11-16 00:09:58 +0530

    Update features.py

--- Advanced Stock Prediction Dashboard/features.py
+++ Advanced Stock Prediction Dashboard/features.py
@@ -20,3 +20,5 @@ def rolling_volatility(series, window=20):
 def macd(series, fast=12, slow=26, signal=9):
     ema_fast = series.ewm(span=fast, adjust=False).mean()
     ema_slow = series.ewm(span=slow, adjust=False).mean()
+    macd_line = ema_fast - ema_slow
+    signal_line = macd_line.ewm(span=signal, adjust=False).mean()
