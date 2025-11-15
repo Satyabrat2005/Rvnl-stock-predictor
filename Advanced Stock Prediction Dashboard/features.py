049388df210d8ae7e70c07ce298326d7f86b1d21
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-11-16 00:10:12 +0530

    Update features.py

--- Advanced Stock Prediction Dashboard/features.py
+++ Advanced Stock Prediction Dashboard/features.py
@@ -22,3 +22,5 @@ def macd(series, fast=12, slow=26, signal=9):
     ema_slow = series.ewm(span=slow, adjust=False).mean()
     macd_line = ema_fast - ema_slow
     signal_line = macd_line.ewm(span=signal, adjust=False).mean()
+    hist = macd_line - signal_line
+    return macd_line, signal_line, hist
