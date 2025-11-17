3ad6d2e6e0ef02388f762476088e28214f2ed069
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-11-17 22:18:42 +0530

    Add position management for trading strategy

--- Advanced Stock Prediction Dashboard/strategies.py
+++ Advanced Stock Prediction Dashboard/strategies.py
@@ -10,3 +10,7 @@ def ema_rsi_vol_signals(df, rsi_high=70, vol_thresh=0.6):
   ema_fast, ema_slow = df['ema_fast'], df['ema_slow']
   cross_up = (ema_fast > ema_slow) & (ema_fast.shift(1) <= ema_slow.shift(1))
   cross_down = (ema_fast < ema_slow) & (ema_fast.shift(1) >= ema_slow.shift(1))
+  position = 0
+    for t in df.index:
+        if position == 0 and cross_up.loc[t] and df.loc[t,'rsi']<rsi_high and df.loc[t,'vol']<vol_thresh:
+            position = 1
