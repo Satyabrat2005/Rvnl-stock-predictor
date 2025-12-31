6fda873d6f2b6ee0a9b21f113e896cc24ba714e2
Satyabrat2005 <satyabratsahu71@gmail.com>
2025-12-31 20:07:14 +0530

    Update strategies.py

--- Advanced Stock Prediction Dashboard/strategies.py
+++ Advanced Stock Prediction Dashboard/strategies.py
@@ -1,59 +1,12 @@
 import pandas as pd
+import numpy as np 
 
-# EMA + RSI + Volatility
-def ema_rsi_vol_signals(df, rsi_high=70, vol_thresh=0.6):
-  """
-  Generate long/flat signals based on EMA crossover + RSI + Volatility filter
-  """
-  s = pd.Series(index=df.index, dtype=float)
-  s[:] = 0.0
-  ema_fast, ema_slow = df['ema_fast'], df['ema_slow']
-  cross_up = (ema_fast > ema_slow) & (ema_fast.shift(1) <= ema_slow.shift(1))
-  cross_down = (ema_fast < ema_slow) & (ema_fast.shift(1) >= ema_slow.shift(1))
-  position = 0
-    for t in df.index:
-        if position == 0 and cross_up.loc[t] and df.loc[t,'rsi']<rsi_high and df.loc[t,'vol']<vol_thresh:
-            position = 1
-        elif position == 1 and (cross_down.loc[t] or df.loc[t,'rsi']>rsi_high or df.loc[t,'vol']>vol_thresh):
-            position = 0
-        s.loc[t] = position
+def apply_cooldown(signal: pd.Series, cooldown_days: int = 5):
+    s = signal.copy()
+    last_trade = cooldown_days
+    for i in range(len(s)):
+        if s.iloc[i] == 1 and i - last_trade < cooldown_days:
+            s.iloc[i] = 0
+        elif s.iloc[i] == 1:
+            last_trade = i
     return s
-
-def bollinger_signals(df, lookback=20, n_std=2):
-    """
-    Buy when price crosses below lower band, sell/exit when price crosses above SMA
-    """
-    s = pd.Series(index=df.index, dtype=float)
-    s[:] = 0.0
-    position = 0
-    for t in df.index:
-        price = df.loc[t,'Close']
-        lower = df.loc[t,'bb_lower']
-        upper = df.loc[t,'bb_upper']
-        sma = (upper + lower)/2  
-
-        if position == 0 and price < lower:
-            position = 1  # buy
-        elif position == 1 and price > sma:
-            position = 0  # exit
-        s.loc[t] = position
-    return s
-
-# MACD Trend Following
-def macd_signals(df):
-    """
-    Buy when MACD line crosses above signal line, exit when crosses below
-    """
-    s = pd.Series(index=df.index, dtype=float)
-    s[:] = 0.0
-    macd, signal = df['macd'], df['macd_signal']
-    cross_up = (macd > signal) & (macd.shift(1) <= signal.shift(1))
-    cross_down = (macd < signal) & (macd.shift(1) >= signal.shift(1))
-    position = 0
-    for t in df.index:
-        if position == 0 and cross_up.loc[t]:
-            position = 1
-        elif position == 1 and cross_down.loc[t]:
-            position = 0
-        s.loc[t] = position
-     return s
