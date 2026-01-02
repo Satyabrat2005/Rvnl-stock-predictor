edf2d0efbadb94350979adba237173a67515c00d
Satyabrat2005 <satyabratsahu71@gmail.com>
2026-01-02 21:51:49 +0530

    Add docstring to fetch_price_data function
    Added docstring to fetch_price_data function.

--- Advanced Stock Prediction Dashboard/Data.py
+++ Advanced Stock Prediction Dashboard/Data.py
@@ -2,4 +2,8 @@ import yfinance as yf
 import pandas as pd
 
 
-def fetch_price_data(tickers, start, end):
+def fetch_price_data(tickers, start, end
+    """
+    Fetch OHLCV data reliably (works better for NSE stocks than yf.download)
+    """
+    all_data = {}
